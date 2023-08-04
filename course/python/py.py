d2e19d6062aed23977476acdf82cc627380357fd
WraithLyd <ZJUWraith@outlook.com>
2023-08-05 01:11:09 +0800

    Deployed 320f2c7 with MkDocs version: 1.5.1

---
+++ course/python/py.py
@@ -0,0 +1,17 @@
+print(type("hello"))
+# <class >
+print(type(124))
+print("Hello\npython")
+
+print("name\tage\taddress")
+print("Tom\t8\tWhite House")
+print("Jerry\t8\tWhite House")
+
+print("""\
+壮岁旌旗拥万夫
+锦襜突骑渡江初
+燕兵夜娖银胡䩮
+汉箭朝飞金仆姑\
+""")
+
+print(len("Hello world!"))
